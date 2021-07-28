0c71f99134c48fc01cf8b74edf19932ea0848196
moo-the-cow <info@edmail.at>
2021-07-28 12:08:10 +0200

    testing jsonp

--- mooderation.js
+++ mooderation.js
@@ -354,14 +354,14 @@ function websocketConnect() {
 			$.ajax({
 				Url: `${env.data.twitchUsersUrl}/group/user/${config.channel}/chatters`,
 				type: "get",
-				dataType: "json",
+				dataType: "jsonp",
 				jsonp: "callback",
 				jsonpCallback:"jsonpCallback",
 				beforeSend:function(){
 					console.log("loading before send");
 				},
 				success: function (data) {
-					console.log(data);
+					console.log(JSON.stringify(data));
 				},
 				error: function () {
 					console.log("system error");
